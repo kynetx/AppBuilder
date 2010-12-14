9101a5d386ac3517b502fd2b71f9e5316b03c455
Alex Olson <einstein.ako@gmail.com>
2010-12-14 13:47:46 -0700

    Adding bundler, applist button

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# A sample Gemfile
+source "http://rubygems.org"
+
+gem "rails", "2.3.8"
+gem "authlogic"
+gem "httparty"
+gem "oauth"
+gem "kynetx_am_api"
+gem "multipart-post", :require => "net/http/post/multipart"
+gem "sqlite3-ruby", :require => "sqlite3"
--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,25 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
+
+class Rails::Boot
+  def run
+    load_initializer
+
+    Rails::Initializer.class_eval do
+      def load_gems
+        @bundler_loaded ||= Bundler.require :default, Rails.env
+      end
+    end
+
+    Rails::Initializer.run(:set_load_path)
+  end
+end
+
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
@@ -19,11 +34,11 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "authlogic"
-  config.gem "httparty"
-  config.gem "oauth"
-  config.gem "kynetx_am_api"
-  config.gem "multipart-post", :lib => "net/http/post/multipart"
+  #config.gem "authlogic"
+  #config.gem "httparty"
+  #config.gem "oauth"
+  #config.gem "kynetx_am_api"
+  #config.gem "multipart-post", :lib => "net/http/post/multipart"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
